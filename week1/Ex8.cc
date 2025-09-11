aee16039cb7ba7a29aec32721734db07d0ea1b7a
bulfan <tudorbulf@gmail.com>
2025-09-11 22:09:54 +0200

    added a comment to ex8 to answer their question

--- week1/Ex8.cc
+++ week1/Ex8.cc
@@ -15,3 +15,9 @@ int main(int argc, char* argv[])
          << (ip4 >> 8 & 0xFF) << "." 
          << (ip4 & 0xFF) << "\n";
 }
+// the last 2 outputs are the same because the 32-bit representation of 
+// 129.125.3.162 is 10000001 01111101 00000011 10100010. Applying a 20 bit
+// netmask means that the last 12 bits are set to 0. Applying a 16 bit netmask
+// means that the last 16 bits are set to 0. Since bits 17-22 are 0, this
+// means that by applying a netmask of 20 or 16 (or anything between 16 and 22)
+// will result in the same output.
