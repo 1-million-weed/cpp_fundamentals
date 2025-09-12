4579ca73f32f603c6bcb2efcba46d90612299c2b
Rob-Sligter <r.g.l.sligter@student.rug.nl>
2025-09-12 22:36:48 +0200

    Message exercise 5

--- set1/05/05.cc
+++ set1/05/05.cc
@@ -2,5 +2,5 @@
 
 int main() 
 {
-    std::cout << "Hello\cWorld\n";
+    std::cout << "Hello\cWorld\n"; // This will give a compiler warning because we are demonstrating the use of an unknown escape sequence.
 }
