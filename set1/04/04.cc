db984e83aac1b35904b8190bd83853658a774994
bulfan <tudorbulf@gmail.com>
2025-09-12 19:33:27 +0200

    formated everything

--- set1/04/04.cc
+++ set1/04/04.cc
@@ -8,11 +8,7 @@ R"R(
 ^\s+ssid="([[:print:]]+)"
 )R";
 
-// R"(...)"  can be used when the string
-// you want to output does not contain )"
-// otherwise your string will end prematurely
-
 int main()
 {
-     std::cout <<text;
+     std::cout << text << '\n';
 }
--- set1/08/08.cc
+++ set1/08/08.cc
@@ -5,19 +5,18 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+    // convert the octets into an unsigned value and combine them into a single 32-bit value
     size_t ip4 = (stoul(argv[1]) << 24) | (stoul(argv[2]) << 16) | (stoul(argv[3]) << 8) | stoul(argv[4]);
+    // initialise all bits of the mask to 1
     size_t mask = ~0UL;
     size_t netmask = stoul(argv[5]);
+    // shift left to keep the upper netmask bits
     mask <<= (32 - netmask);
+    // zero out the bits
     ip4 &= mask;
+    // print the network addres in the dotted format
     cout << (ip4 >> 24) << "." 
          << (ip4 >> 16 & 0xFF) << "." 
          << (ip4 >> 8 & 0xFF) << "." 
          << (ip4 & 0xFF) << "\n";
 }
-// the last 2 outputs are the same because the 32-bit representation of 
-// 129.125.3.162 is 10000001 01111101 00000011 10100010. Applying a 20 bit
-// netmask means that the last 12 bits are set to 0. Applying a 16 bit netmask
-// means that the last 16 bits are set to 0. Since bits 17-22 are 0, this
-// means that by applying a netmask of 20 or 16 (or anything between 16 and 22)
-// will result in the same output.
