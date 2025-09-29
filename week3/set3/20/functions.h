e5fc318e52a9c1b2f722da46a51339df82dfcff3
Rob-Sligter <r.g.l.sligter@student.rug.nl>
2025-09-29 18:46:54 +0200

    fixed function

---
+++ week3/set3/20/functions.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <string>
+
+// Function declarations
+int addNumbers(int a, int b);
+std::string greetUser(const std::string& name);
+double calculateAverage(double arr[], int size);
+
+#endif
