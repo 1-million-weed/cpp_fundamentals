ee71877a15ac5cac5d6f05a02437589187958772
bulfan <tudorbulf@gmail.com>
2025-09-29 21:15:52 +0200

    the so called week3 commit

--- week3/set3/18/main.cc
+++ week3/set3/18/main.cc
@@ -1,16 +1,16 @@
-int main(int argc, char *argv[]) {
-    if (argc == 1) {
-        std::cout << "The program was run with no arguments. Usage: >/[program name] [arg1] [arg2] ..." << std::endl;
+#include "main.h"
+
+int main(int argc, char *argv[]) 
+{
+    if (argc == 1) 
+    {
+        cout << "The program was run with no arguments. Usage: ./[program name] [arg1] [arg2] ...\n";
         return 1;
     }
 
-    // Call structCall first; it will print the result and terminate the program if false
-    if (!structCall(argc, argv)) {
-        return 1;
-    }
+    if (!structCall(argc, argv))
+        return 1;  // we terminate the program if the function returns false
 
-    // Call boundCall second; it also prints the result
     boundCall(argc, argv);
-
     return 0;
 }
--- week3/set3/18/main.h
+++ week3/set3/18/main.h
@@ -1,2 +1,18 @@
-// i tried naming this function with a .ih, but it doesn t work
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <string>
+#include <iostream>
+using namespace std;
+
+struct ReturnValues 
+{
+    bool ok;
+    size_t nr;
+    std::string value;
+};
+bool combine(int argc, char *argv[]);
+bool structCall(int argc, char *argv[]);
+void boundCall(int argc, char *argv[]);
+
+#endif
